809fb5fae97dc83e06f780081dbb64aa144c2e0a
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-09-09 11:22:30 -0500

    done with top part

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import logo from './images/logo.svg'
 import searchlogoblack from './images/searchblack.svg'
 import searchlogowhite from './images/searchwhite.svg'
+import cart from './images/cart.svg'
 
 import cancelicon from './images/cancelicon.svg'
 import { useState } from 'react';
@@ -12,12 +13,16 @@ const [hoversearch, sethoversearch] = useState(false)
       <div className='navbar'>
         <div className='above'>
           <img src={logo} width='80px' alt='etsy logo' />
-          <div className='searchbar'>
-            <input className='searchfield'></input>
+          <div className='searchbar'  onMouseEnter={()=> sethoversearch(true)} onMouseLeave={()=> sethoversearch(false)}>
+            <input className='searchfield' ></input>
             <div>
             <img src={cancelicon} alt='searchlogo' className='cancelicon' />
-            <img src={hoversearch === true ?searchlogowhite:searchlogoblack} onMouseEnter={()=> sethoversearch(true)} onMouseLeave={()=> sethoversearch(false)} alt='searchlogo'className='searchicon' />
+            <img src={hoversearch === true ?searchlogowhite:searchlogoblack} alt='searchlogo'className='searchicon' />
             </div>
+          </div>
+          <div className='icon'>
+          <h2 className='signin'>Sign in</h2>
+          <img src={cart} alt='cart'/>
           </div>
         </div>
         <div className='below'>
